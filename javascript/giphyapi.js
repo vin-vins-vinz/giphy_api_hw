0742bd1b083c4e47f70f7d0d381e4989fa9ece3e
Liang <Nicky@Liangs-Air.fios-router.home>
2017-06-05 22:52:00 -0700

    refreshing, last commit did not work

---
+++ javascript/giphyapi.js
@@ -0,0 +1,70 @@
+
+var topics = ["Chris Paul", "Deron Williams", "Kobe Bryant", "Michael Jordan", "Kyrie Irving"];
+
+function renderButtons (){
+	$("#player-buttons").empty();
+	
+	for (i = 0; i < topics.length; i ++) {
+		var a = $("<button>");
+		a.addClass("players");
+		a.attr("data-name", topics[i]);
+		a.text(topics[i]);
+		$("#player-buttons").append(a);	
+	}
+}
+
+renderButtons();
+
+
+function displayGif(){
+
+	var gifPlayer = $(this).attr("data-name");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifPlayer + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response){
+
+		var results = response.data;
+
+		// console.log(results[1].rating);
+
+		for (n=0; n<results.length; n++){
+
+			if (results[n].rating !== "g" && results[n].rating !== "pg" && results[n].rating !== "pg-13" && results[n].rating !== "y") {
+
+				var gifDiv = $("<div class='resultGif'>");
+
+				var gifRating = results[n].rating;
+				var ratingP = $("<p>").text("Rating: " + gifRating);
+
+				// var imageUrl = results[j].images.fixed_height_small_still.url;
+				var gifStill = $("<img>");
+				gifStill.attr("src", results[n].images.fixed_height_small_still.url);
+				// gifStill.attr("alt", "image loading");
+				// console.log(gifStill);
+				gifDiv.append(ratingP);
+				gifDiv.append(gifStill);
+
+				$("#gif-display").prepend(gifDiv);
+
+			}
+		}
+	});
+
+
+};
+
+$("#add-player").on("click",function(event){
+	event.preventDefault();
+	var newPlayer = $("#player-input").val().trim();
+	topics.push(newPlayer);
+	renderButtons();
+});
+
+$(document).on("click", "#gif-display", displayGif);
+
+renderButtons();
+
+
